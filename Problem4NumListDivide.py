a2846cad92aca8e0e5763324a37486c16cf2da90
Dlopez2990 <139589356+Dlopez2990@users.noreply.github.com>
2023-08-31 14:14:06 -0500

    Create Problem4NumListDivide.py

---
+++ Problem4NumListDivide.py
@@ -0,0 +1,13 @@
+#Daniel Lopez
+#8/31/23
+#Problem4NumListDivide.py
+#Create a while loop that initializes a counter at 0 and will run until the counter reaches 50. If the value of the counter is divisible by 10, append the value to the list called tens. Confirm the list results using a print statement.
+counter = 0
+tens = []
+
+while counter <= 50:
+    if counter % 10 == 0:
+        tens.append(counter)
+    counter += 1
+
+print("List of numbers divisible by 10:", tens)
