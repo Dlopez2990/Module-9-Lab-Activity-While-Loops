331667e2ea88f92c3c8ec5e823bbaf5ca3353931
Dlopez2990 <139589356+Dlopez2990@users.noreply.github.com>
2023-08-31 14:12:03 -0500

    Create Problem1Infinite.py

---
+++ Problem1Infinite.py
@@ -0,0 +1,6 @@
+#Daniel L
+#8/31/23
+#Problem1Infinite.py
+#Write an infinite loop that prints “Infinite”. An infinite loop never ends. The condition is always true
+while True:
+    print("Infinite")
